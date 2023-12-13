3ed3f040046061daaa965d2cf44a600440c23204
Melissa Ruiz <melruizglez97@gmail.com>
2023-12-13 23:37:25 +0000

    up

---
+++ Primero/PRO/proyecto-test/trabajo-metodos-test/src/main/java/ies/puerto/Ejercicio6.java
@@ -0,0 +1,25 @@
+package ies.puerto;
+
+/**
+ * marco de cuadro 15 x 5
+ */
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 15; i++) {
+            System.out.print("1");
+        }
+        System.out.println("");
+        for (int j = 0; j < 3; j++) {
+            System.out.print("1");
+            for (int k = 0; k < 13; k++) {
+                System.out.print(" ");
+            }
+            System.out.print("1");
+            System.out.println("");
+        }
+        for (int i = 0; i < 15; i++) {
+            System.out.print("1");
+        }
+        System.out.println("");
+    }
+}
